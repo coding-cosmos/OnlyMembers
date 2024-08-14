2792673f30c561151fea2cc7cea3fa399347037e
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-08-15 00:06:39 +0530

    Add findUser() and findUserById()

--- db/queries.js
+++ db/queries.js
@@ -7,4 +7,14 @@ async function insertUser(firstname, lastname, username, password) {
   );
 }
 
-export default { insertUser };
+async function findUser(username) {
+  const {rows} = await pool.query('SELECT * FROM users WHERE username = ($1)',[username]);
+  return rows[0];
+}
+
+async function findUserById(id) {
+  const {rows} = await pool.query('SELECT * FROM users WHERE id = ($1)',[id]);
+  return rows[0];
+}
+
+export default { insertUser ,findUser,findUserById};
