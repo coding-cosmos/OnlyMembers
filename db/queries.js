e2b8fd4f9412dba75d5d5987b677b2fec2de8d35
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-08-14 21:51:36 +0530

    Add insertUser function

--- db/queries.js
+++ db/queries.js
@@ -1,2 +1,10 @@
-import pool from './pool.js';
+import pool from "./pool.js";
 
+async function insertUser(firstname, lastname, username, password) {
+  await pool.query(
+    "INSERT INTO users(first_name,last_name,username,password) VALUES ($1,$2,$3,$4)",
+    [firstname, lastname, username, password]
+  );
+}
+
+export default { insertUser };
