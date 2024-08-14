7e07fbd2fb42cbd2d3fd8813edee999dfbdad473
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-08-14 21:49:50 +0530

    Add route for log-in

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import {
   indexPageGet,
   signUpGet,
   signUpPost,
+  logInGet
 } from "../controllers/indexController.js";
 
 const indexRouter = express.Router();
@@ -10,5 +11,6 @@ const indexRouter = express.Router();
 indexRouter.get("/", indexPageGet);
 indexRouter.get("/sign-up", signUpGet);
 indexRouter.post("/sign-up", signUpPost);
+indexRouter.get('/log-in',logInGet);
 
 export default indexRouter;
