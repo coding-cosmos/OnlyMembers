55f3e8f2c96cb8e1611da35e1fc52850d34429a9
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-08-15 17:52:00 +0530

    Add addPost() and getPosts()

--- db/queries.js
+++ db/queries.js
@@ -21,4 +21,16 @@ async function changeStatus(id,status) {
   await pool.query('UPDATE users SET status = ($2) WHERE id = ($1)',[id,status]);
 }
 
-export default { insertUser ,findUser,findUserById,changeStatus};
+async function addPost(title,message,user_id) {
+  await pool.query(
+    "INSERT INTO posts(title,message,user_id) VALUES ($1,$2,$3)",
+    [title, message, user_id]
+  );
+}
+
+async function getPosts(){
+  const {rows} = await pool.query('SELECT * FROM posts INNER JOIN users ON posts.user_id = users.id');
+  return rows;
+}
+
+export default { insertUser ,findUser,findUserById,changeStatus,addPost,getPosts};
