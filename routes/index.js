30a4443d0d6835224f899fd5f77b70374efc4c43
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-08-15 14:48:09 +0530

    Add logut route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ import {
   signUpPost,
   logInGet,
   logInPost,
+  logOut,
 } from "../controllers/indexController.js";
 
 const indexRouter = express.Router();
@@ -14,5 +15,6 @@ indexRouter.get("/sign-up", signUpGet);
 indexRouter.post("/sign-up", signUpPost);
 indexRouter.get("/log-in", logInGet);
 indexRouter.post("/log-in", logInPost);
+indexRouter.get('/log-out',logOut);
 
 export default indexRouter;
